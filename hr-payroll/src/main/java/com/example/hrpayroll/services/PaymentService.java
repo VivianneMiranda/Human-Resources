0a9feecc43b121bffcd619c69472180b2cbd4460
Vivianne Miranda <vivianne.chaves17@gmail.com>
2021-09-19 21:18:44 -0300

    Feign Client

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -1,14 +1,23 @@
 package com.example.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.hrpayroll.entities.Payment;
+import com.example.hrpayroll.entities.Worker;
+import com.example.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Vivi", 300.0, days);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
